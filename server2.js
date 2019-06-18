2c77a4bed51e8d2ffac4905fb7a3593d67dbfb80
yevheniia <jene4ka1818@gmail.com>
2019-06-17 21:36:35 -0400

    dd

--- server2.js
+++ server2.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3500;
+var PORT = process.env.PORT || 3500;
 
 // Initialize Express
 var app = express();
